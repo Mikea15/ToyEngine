0207616f57e8f1bf19a61c7c01cee0d432739b21
Michael Adaixo <michael.adaixo@gmail.com>
2020-04-12 21:21:15 +0100

    boid update and group behavior setup

--- BoidSystem/BoidSystemState.h
+++ BoidSystem/BoidSystemState.h
@@ -6,153 +6,292 @@
 
 class Game;
 
-#define ENTITY_COUNT 10000
+#define ENTITY_COUNT 100
+
+struct Path
+{
+    Path() = default;
+    Path(std::vector<glm::vec3> path)
+    {
+        m_nodes = path;
+        m_currentNodeIndex = static_cast<unsigned int>(MathUtils::Rand(0, m_nodes.size() - 1));
+    }
+
+    glm::vec3 GetCurrentGoal() const { return m_nodes[m_currentNodeIndex]; }
+    void UpdatePath( glm::vec3 position )
+    {
+        float distanceToGoal = glm::length(GetCurrentGoal() - position);
+        if (distanceToGoal <= atGoalDistanceThreshold)
+        {
+            m_currentNodeIndex++;
+            if (m_currentNodeIndex > m_nodes.size() - 1) 
+            {
+                m_currentNodeIndex = 0;
+            }
+        }
+    }
+
+    void DebugDraw()
+    {
+        if (m_nodes.empty())
+        {
+            return;
+        }
+
+        for (size_t i = 0; i < m_nodes.size(); i++)
+        {
+            if (i == m_currentNodeIndex)
+            {
+                DebugDraw::AddAABB(
+                    m_nodes[i] - glm::vec3(atGoalDistanceThreshold), 
+                    m_nodes[i] + glm::vec3(atGoalDistanceThreshold),
+                    { 0.3f, 0.6f, 0.1, 1.0f });
+            }
+
+            if (i != m_nodes.size() - 1) 
+            {
+                DebugDraw::AddLine(m_nodes[i], m_nodes[i + 1], { 0.3f, 0.6f, 0.1, 1.0f });
+            }
+        }
+        DebugDraw::AddLine(m_nodes[m_nodes.size() - 1], m_nodes[0], { 0.3f, 0.6f, 0.1, 1.0f });
+    }
+
+    size_t m_currentNodeIndex;
+    std::vector<glm::vec3> m_nodes;
+
+    float atGoalDistanceThreshold = 1.0f;
+};
 
 class BoidSystemState
-	: public State
+    : public State
 {
 public:
-	BoidSystemState()
-		: State()
-	{}
+    BoidSystemState()
+        : State()
+    {}
 
-	~BoidSystemState() override {};
+    ~BoidSystemState() override {};
 
-	void Init(Game* game) override
-	{
-		gamePtr = game;
-		m_renderer = gamePtr->GetRenderer();
+    void Init(Game* game) override
+    {
+        gamePtr = game;
+        m_renderer = gamePtr->GetRenderer();
 
-		// configure camera
-		m_camera = FlyCamera(glm::vec3(0.0f, 2.0f, 10.0f));
-		m_camera.SetPerspective(glm::radians(90.0f),
-			static_cast<float>(m_renderer->GetRenderWidth() / m_renderer->GetRenderHeight()),
-			0.01f, 200.0f);
+        // configure camera
+        m_camera = FlyCamera(glm::vec3(0.0f, 2.0f, 10.0f));
+        m_camera.SetPerspective(glm::radians(90.0f),
+            static_cast<float>(m_renderer->GetRenderWidth() / m_renderer->GetRenderHeight()),
+            0.01f, 200.0f);
 
-		m_renderer->SetCamera(&m_camera);
+        m_renderer->SetCamera(&m_camera);
 
-		m_sphere = new Sphere(32, 32);
-		Material* defaultForwardMat = m_renderer->CreateMaterial("default-fwd");
+        m_sphere = new Sphere(32, 32);
+        Material* defaultForwardMat = m_renderer->CreateMaterial("default-fwd");
 
-		m_viewGrid = ViewportGrid(100, 100, 100, 100);
+        m_viewGrid = ViewportGrid(100, 100, 100, 100);
 
-		DebugDraw::Init();
-	};
+        for (size_t i = 0; i < 15; i++)
+        {
+            m_randomPositions.push_back(glm::vec3(
+                MathUtils::Rand(-10.0f, 10.0f),
+                MathUtils::Rand(-10.0f, 10.0f),
+                MathUtils::Rand(-10.0f, 10.0f)
+            ));
+        }
 
-	void HandleInput(SDL_Event* event) override
-	{
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_F1)
-		{
-			m_inputGrabMouse = !m_inputGrabMouse;
-			SDL_SetRelativeMouseMode(m_inputGrabMouse ? SDL_TRUE : SDL_FALSE);
-		}
+        m_path = m_randomPositions;
 
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_w) m_inputMoveForward = 1;
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_s) m_inputMoveForward = -1;
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_a) m_inputMoveRight = -1;
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_d) m_inputMoveRight = 1;
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_e) m_inputMoveUp = 1;
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_q) m_inputMoveUp = -1;
-		if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_LSHIFT) m_inputEnableMovementBoost = true;
+        for (size_t i = 0; i < ENTITY_COUNT; i++)
+        {
+            auto b = Boid();
+            auto features = Boid::eAlignment | Boid::eSeparation | Boid::eCohesion;
+            b.SetFeature(features);
+            // b.m_maxAccelerationForce = MathUtils::Rand01() * 0.1f;
+            // b.m_maxVelocity = MathUtils::Rand01() * 5.0f;
+            b.m_position = glm::vec3(
+                MathUtils::Rand(-10.0f, 10.0f),
+                MathUtils::Rand(-10.0f, 10.0f),
+                MathUtils::Rand(-10.0f, 10.0f)
+            );
 
-		if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_w) m_inputMoveForward = 0;
-		if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_s) m_inputMoveForward = 0;
-		if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_a) m_inputMoveRight = 0;
-		if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_d) m_inputMoveRight = 0;
-		if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_e) m_inputMoveUp = 0;
-		if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_q) m_inputMoveUp = 0;
-		if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_LSHIFT) m_inputEnableMovementBoost = false;
-	};
+            b.SetTarget(&m_simplePathFollower);
 
-	void Update(float deltaTime) override
-	{
-		glm::vec3 mouseWorldPos = {};
-		if (m_inputGrabMouse) {
-			int x, y;
-			SDL_GetRelativeMouseState(&x, &y);
-			m_camera.HandleMouse(static_cast<float>(x), static_cast<float>(-y));
+            m_wanderers.push_back(b);
+        }
 
-			mouseWorldPos = m_camera.ScreenSpaceToWorldSpace(
-				glm::vec3(static_cast<float>(x), static_cast<float>(-y), 0.f)
-			);
-		}
+        m_simpleFollower.SetFeature(Boid::eSeek);
+        m_simplePathFollower.SetFeature(Boid::eSeek);
 
-		// get camera movement input
-		glm::vec3 inputDir(m_inputMoveRight, m_inputMoveUp, m_inputMoveForward);
-		m_camera.HandleMove(deltaTime, inputDir, m_inputEnableMovementBoost);
+        m_simpleArriver.SetFeature(Boid::eArrive);
 
-		m_camera.Update(deltaTime);
+        m_simpleFlee.SetFeature(Boid::eFleeRanged);
+        m_simpleFlee.SetFlee(&m_simpleFollower);
 
-		// Debug
-		DebugDraw::Clear();
+        DebugDraw::Init();
+    };
 
-		glm::mat4 viewProj = m_camera.GetViewProjection();
-		DebugDraw::Update(viewProj);
+    void HandleInput(SDL_Event* event) override
+    {
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_F1)
+        {
+            m_inputGrabMouse = !m_inputGrabMouse;
+            SDL_SetRelativeMouseMode(m_inputGrabMouse ? SDL_TRUE : SDL_FALSE);
+        }
 
-		time += deltaTime;
-		stopTimer -= deltaTime;
-		
-		if (stopTimer < 0.0f)
-		{
-			stopTimer = stopInterval;
-			m_movingTarget = glm::normalize(glm::vec3(
-				1.0f - sin(time) * 2.0f, 
-				1.0f - cos(time) * 2.0f, 
-				0.0f
-			)) * 4.0f;
-		}
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_w) m_inputMoveForward = 1;
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_s) m_inputMoveForward = -1;
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_a) m_inputMoveRight = -1;
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_d) m_inputMoveRight = 1;
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_e) m_inputMoveUp = 1;
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_q) m_inputMoveUp = -1;
+        if (event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_LSHIFT) m_inputEnableMovementBoost = true;
 
-		DebugDraw::AddPosition(m_movingTarget, 0.5f, { 0.6f, 0.2f, 0.4f, 1.0f });
+        if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_w) m_inputMoveForward = 0;
+        if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_s) m_inputMoveForward = 0;
+        if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_a) m_inputMoveRight = 0;
+        if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_d) m_inputMoveRight = 0;
+        if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_e) m_inputMoveUp = 0;
+        if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_q) m_inputMoveUp = 0;
+        if (event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_LSHIFT) m_inputEnableMovementBoost = false;
+    };
 
-		m_viewGrid.Draw();
+    void Update(float deltaTime) override
+    {
+        glm::vec3 mouseWorldPos = {};
+        if (m_inputGrabMouse) {
+            int x, y;
+            SDL_GetRelativeMouseState(&x, &y);
+            m_camera.HandleMouse(static_cast<float>(x), static_cast<float>(-y));
 
-		// m_simpleBoid.Seek(m_movingTarget);
-		m_simpleBoid.Arrive(m_movingTarget);
-		m_simpleBoid.Update(deltaTime);
+            mouseWorldPos = m_camera.ScreenSpaceToWorldSpace(
+                glm::vec3(static_cast<float>(x), static_cast<float>(-y), 0.f)
+            );
+        }
 
-		DebugDraw::AddPosition(m_simpleBoid.m_position, 0.5f, { 0.2f, 0.5f, 0.5f, 1.0f });
-		DebugDraw::AddLine(m_simpleBoid.m_position, m_simpleBoid.m_position + m_simpleBoid.m_velocity, { 0.0f, 0.0f, 1.0f, 1.0f });
-		DebugDraw::AddAABB(m_simpleBoid.m_position - glm::vec3(m_simpleBoid.m_radius), m_simpleBoid.m_position + glm::vec3(m_simpleBoid.m_radius), { 0.2f, 0.5f, 0.5f, 1.0f });
-	};
+        // get camera movement input
+        glm::vec3 inputDir(m_inputMoveRight, m_inputMoveUp, m_inputMoveForward);
+        m_camera.HandleMove(deltaTime, inputDir, m_inputEnableMovementBoost);
 
-	void Render(float alpha = 1.0f) override
-	{
+        m_camera.Update(deltaTime);
 
+        // Debug
+        DebugDraw::Clear();
+
+        glm::mat4 viewProj = m_camera.GetViewProjection();
+        DebugDraw::Update(viewProj);
+
+        time += deltaTime;
+        stopTimer -= deltaTime;
+
+        if (stopTimer < 0.0f)
+        {
+            stopTimer = stopInterval;
+            m_movingTarget = glm::normalize(glm::vec3(
+                1.0f - sin(time) * 2.0f,
+                1.0f - cos(time) * 2.0f,
+                0.0f
+            )) * 4.0f;
+        }
+
+        m_constantMovingTarget = glm::normalize(glm::vec3(
+            sin(time * 0.5f),
+            cos(time * 0.5f),
+            0.0f
+        )) * 4.0f;
+
+        DebugDraw::AddAABB(
+            m_movingTarget - glm::vec3(0.1f),
+            m_movingTarget + glm::vec3(0.1f), { 0.7f, 0.3f, 0.4f, 1.0f });
+
+        DebugDraw::AddAABB(
+            m_constantMovingTarget - glm::vec3(0.1f),
+            m_constantMovingTarget + glm::vec3(0.1f), { 0.7f, 0.3f, 0.4f, 1.0f });
+
+
+        // DebugDraw::AddPosition(m_movingTarget, 0.5f, { 0.6f, 0.2f, 0.4f, 1.0f });
+        // DebugDraw::AddPosition(m_constantMovingTarget, 0.5f, { 0.6f, 0.2f, 0.4f, 1.0f });
+
+        m_viewGrid.Draw();
+
+        for (size_t i = 0; i < ENTITY_COUNT; i++)
+        {
+            m_wanderers[i].Update(deltaTime, m_wanderers);
+            m_wanderers[i].DrawDebug();
+        }
+
+        {
+            m_path.UpdatePath(m_simplePathFollower.m_position);
+            m_path.DebugDraw();
+
+            m_simplePathFollower.SetTarget(m_path.GetCurrentGoal());
+            m_simplePathFollower.Update(deltaTime, m_wanderers);
+            m_simplePathFollower.DrawDebug();
+        }
+
+        {
+            m_simpleFollower.SetTarget(m_constantMovingTarget);
+            m_simpleFollower.Update(deltaTime, m_wanderers);
+            m_simpleFollower.DrawDebug();
+        }
+
+        {
+            m_simpleArriver.SetTarget(m_movingTarget);
+            m_simpleArriver.Update(deltaTime, m_wanderers);
+            m_simpleArriver.DrawDebug();
+        }
+
+        {
+            m_simpleFlee.Update(deltaTime, m_wanderers);
+            m_simpleFlee.DrawDebug();
+        }
+    };
+
+    void Render(float alpha = 1.0f) override
+    {
 #if 0
-		for (SceneNode* node : m_sceneNodes)
-		{
-			m_renderer->PushRender(node);
-		}
+        for (SceneNode* node : m_sceneNodes)
+        {
+            m_renderer->PushRender(node);
+        }
 #endif
-		m_renderer->RenderPushedCommands();
+        m_renderer->RenderPushedCommands();
 
-		DebugDraw::AddPosition(glm::vec3(0.0f), 0.5f);
+        DebugDraw::AddPosition(glm::vec3(0.0f), 0.5f);
 
-		bool x_ray = false;
-		DebugDraw::Draw(x_ray);
-	};
+        bool x_ray = false;
+        DebugDraw::Draw(x_ray);
+    };
 
-	void RenderUI() {};
-	void Cleanup() override {};
+    void RenderUI() {};
+    void Cleanup() override {};
 
 private:
-	Game* gamePtr;
-	SimpleRenderer* m_renderer;
-	FlyCamera m_camera;
-	ViewportGrid m_viewGrid;
-	// render representation
-	Sphere* m_sphere;
+    Game* gamePtr;
+    SimpleRenderer* m_renderer;
+    FlyCamera m_camera;
+    ViewportGrid m_viewGrid;
+    // render representation
+    Sphere* m_sphere;
 
-	bool m_inputGrabMouse = false;
-	float m_inputMoveUp = 0.0f;
-	float m_inputMoveRight = 0.0f;
-	float m_inputMoveForward = 0.0f;
-	bool m_inputEnableMovementBoost = false;
+    bool m_inputGrabMouse = false;
+    float m_inputMoveUp = 0.0f;
+    float m_inputMoveRight = 0.0f;
+    float m_inputMoveForward = 0.0f;
+    bool m_inputEnableMovementBoost = false;
 
-	Boid m_simpleBoid;
-	glm::vec3 m_movingTarget = { 1.0f, 1.0f, 1.0f };
+    Boid m_simpleFollower;
+    Boid m_simpleArriver;
+    Boid m_simpleFlee;
+    Boid m_simplePathFollower;
+    glm::vec3 m_movingTarget = { 1.0f, 1.0f, 1.0f };
+    glm::vec3 m_constantMovingTarget = { 1.0f, 1.0f, 1.0f };
 
-	float time = {};
-	float stopTimer = {};
-	float stopInterval = 5.0f;
+    float time = {};
+    float stopTimer = {};
+    float stopInterval = 5.0f;
+
+    std::vector<glm::vec3> m_randomPositions;
+
+    std::vector<Boid> m_wanderers;
+    Path m_path;
 };
--- BoidSystem/Boids/Boid.h
+++ BoidSystem/Boids/Boid.h
@@ -3,78 +3,290 @@
 #include <glm/glm.hpp>
 #include "BoidManager.h"
 
+template<typename E>
+struct EnableBitMaskOperators
+{
+    static const bool enable = false;
+};
+
+template<typename E>
+typename std::enable_if<EnableBitMaskOperators<E>::enable, E>::type
+operator |(E lhs, E rhs)
+{
+    using underlyingType = typename std::underlying_type<E>::type;
+    return static_cast<E>(
+        static_cast<underlyingType>(lhs) |
+        static_cast<underlyingType>(lhs)
+        );
+}
+
+#define ENABLE_BITMASK_OPERATORS(x)  \
+template<>                           \
+struct EnableBitMaskOperators<x>     \
+{                                    \
+	static const bool enable = true; \
+};
+
 struct Boid
 {
-	struct WeightParams
-	{
-		float m_alignment = 1.0f;
-		float m_cohesion = 1.0f;
-		float m_separation = 1.0f;
-	};
+    struct WeightParams
+    {
+        float m_alignment = 1.0f;
+        float m_cohesion = 1.0f;
+        float m_separation = 1.0f;
+    };
 
-	Boid()
-	{
-		m_acceleration = {};
-		m_velocity = {};
-		m_position = {};
+    enum Feature : unsigned int
+    {
+        eNone       = 0,
+        eWander     = 1 << 0,
+        eSeek       = 1 << 1,
+        eFlee       = 1 << 2,
+        eFleeRanged = 1 << 3,
+        eArrive     = 1 << 4,
 
-		m_radius = 1.0f;
-		m_maxSpeed = 3.0f;
-		m_maxForce = 1.0f;
-	}
+        eSeparation = 1 << 5,
+        eCohesion   = 1 << 6,
+        eAlignment  = 1 << 7
+    };
 
-	void Update( float deltaTime )
-	{
-		m_velocity += m_acceleration * deltaTime;
-		m_velocity = glm::clamp(m_velocity, -m_maxSpeed, m_maxSpeed);
+    Boid()
+        : m_id(++ID)
+    {
+        m_velocity = {};
+        m_position = {};
 
-		m_position += m_velocity * deltaTime;
-		m_acceleration = {};
-	}
+        m_radius = 1.0f;
+        m_maxVelocity = 5.0f;
+        m_maxAccelerationForce = 14.0f;
 
-	void Seek(glm::vec3 target)
-	{
-		glm::vec3 desired = target - m_position;
-		desired = glm::normalize(desired);
-		desired *= m_maxSpeed;
+        m_features = eNone;
 
-		glm::vec3 steer = desired - m_velocity;
-		steer = glm::clamp(steer, -m_maxForce, m_maxForce);
+        m_targetBoid = nullptr;
+        m_fleeBoid = nullptr;
+    }
 
-		ApplyForce(steer);
-	}
+    bool operator==(const Boid& rhs) const { return m_id == rhs.m_id; }
+    bool operator!=(const Boid& rhs) const { return m_id != rhs.m_id; }
 
-	void Arrive(glm::vec3 target)
-	{
-		glm::vec3 desired = target - m_position;
-		float distance = glm::length(desired);
-		desired = glm::normalize(desired);
+    void SetTarget(glm::vec3 targetPos) { m_targetPos = targetPos; }
+    void SetTarget(Boid* boid) { m_targetBoid = boid; }
+    void SetFlee(glm::vec3 fleePos) { m_fleePos = fleePos; }
+    void SetFlee(Boid* boid) { m_fleeBoid = boid; }
 
-		float speed = m_maxSpeed;
-		float arriveRadius = 3.0f;
-		if (distance < arriveRadius)
-		{
-			speed = MathUtils::Lerp(0.0f, speed, distance / arriveRadius);
-		}
+    void Update(float deltaTime, std::vector<Boid>& otherBoids)
+    {
+        if (m_targetBoid)
+        {
+            m_targetPos = m_targetBoid->m_position;
+        }
+        if (m_fleeBoid)
+        {
+            m_fleePos = m_fleeBoid->m_position;
+        }
 
-		desired *= speed;
-		glm::vec3 steer = desired - m_velocity;
-		steer = glm::clamp(steer, -m_maxForce, m_maxForce);
+        glm::vec3 accelerationForce = {};
+        if (HasFeature(eWander)) { accelerationForce += Wander(); }
+        if (HasFeature(eSeek)) { accelerationForce += Seek(); }
+        if (HasFeature(eFlee)) { accelerationForce += Flee(); }
+        if (HasFeature(eFleeRanged)) { accelerationForce += FleeRanged(); }
+        if (HasFeature(eArrive)) { accelerationForce += Arrive(); }
 
-		ApplyForce(steer);
-	}
+        if (HasFeature(eSeparation)) { accelerationForce += 0.1f * Separation(otherBoids); }
+        if (HasFeature(eCohesion)) { accelerationForce += Cohesion(otherBoids); }
+        if (HasFeature(eAlignment)) { accelerationForce += Alignment(otherBoids); }
 
-	void ApplyForce(glm::vec3 force)
-	{
-		m_acceleration += force;
-	}
+        accelerationForce += glm::clamp(accelerationForce, -m_maxAccelerationForce, m_maxAccelerationForce);
 
-	glm::vec3 m_position;
-	glm::vec3 m_velocity;
-	glm::vec3 m_acceleration;
+        m_velocity += accelerationForce * deltaTime;
+        m_velocity = glm::clamp(m_velocity, -m_maxVelocity, m_maxVelocity);
 
-	float m_maxSpeed;
-	float m_maxForce;
+        m_position += m_velocity * deltaTime;
+    }
 
-	float m_radius;
+    void DrawDebug()
+    {
+        DebugDraw::AddPosition(m_position, 0.1f, { 0.2f, 0.5f, 0.5f, 1.0f });
+        DebugDraw::AddLine(m_position, m_position + m_velocity * 0.5f, { 0.75f, 0.75f, 0.75f, 1.0f });
+        DebugDraw::AddAABB(m_position - glm::vec3(m_radius), m_position + glm::vec3(m_radius), { 0.2f, 0.5f, 0.5f, 1.0f });
+    }
+
+    glm::vec3 Seek()
+    {
+        glm::vec3 desiredVelocity = glm::normalize(m_targetPos - m_position) * m_maxVelocity;
+        return desiredVelocity - m_velocity;
+    }
+
+    glm::vec3 Flee()
+    {
+        glm::vec3 desiredVelocity = glm::normalize(m_position - m_fleePos) * m_maxVelocity;
+        return desiredVelocity - m_velocity;
+    }
+
+    glm::vec3 FleeRanged()
+    {
+        glm::vec3 desiredVelocity = glm::normalize(m_position - m_fleePos);
+        float distance = glm::length(desiredVelocity);
+        desiredVelocity = glm::normalize(desiredVelocity);
+
+        float maxVelocity = m_maxVelocity;
+        float fleeRadius = 0.8f;
+        if (distance >= fleeRadius)
+        {
+            maxVelocity = 0.0f;
+            // maxVelocity = MathUtils::Lerp(0.0f, m_maxVelocity, 1.0f - (distance / fleeRadius));
+        }
+
+        desiredVelocity *= maxVelocity;
+        return desiredVelocity - m_velocity;
+    }
+
+    glm::vec3 Wander()
+    {
+        float wanderRadius = 2.0f;
+        glm::vec3 jitterDirection = glm::normalize(
+            glm::vec3(
+                MathUtils::Rand01() * 1.0f,
+                MathUtils::Rand01() * 1.0f,
+                MathUtils::Rand01() * 1.0f
+            )) * wanderRadius;
+
+        float wanderDistance = 0.0f;
+        glm::vec3 targetPosJitter = m_position + GetHeading() * wanderDistance + jitterDirection;
+
+        glm::vec3 desiredVelocity = glm::normalize(targetPosJitter - m_position);
+        return desiredVelocity - m_velocity;
+    }
+
+    glm::vec3 Arrive()
+    {
+        glm::vec3 desiredVelocity = m_targetPos - m_position;
+        float distance = glm::length(desiredVelocity);
+        desiredVelocity = glm::normalize(desiredVelocity);
+
+        float speed = m_maxVelocity;
+        float arriveRadius = 4.0f;
+        if (distance <= arriveRadius)
+        {
+            speed = MathUtils::Lerp(0.0f, speed, distance / arriveRadius);
+        }
+
+        desiredVelocity *= speed;
+        return desiredVelocity - m_velocity;
+    }
+
+    glm::vec3 Separation(std::vector<Boid>& neighbors)
+    {
+        glm::vec3 force = {};
+        for (size_t i = 0; i < neighbors.size(); i++)
+        {
+            if (*this == neighbors[i] && glm::length(m_position - neighbors[i].m_position) > m_neighborRange)
+            {
+                continue;
+            }
+
+            if (m_position != neighbors[i].m_position) 
+            {
+                glm::vec3 toAgent = m_position - neighbors[i].m_position;
+                force += glm::normalize(toAgent) / glm::length(toAgent);
+            }
+        }
+
+        return force;
+    }
+
+    glm::vec3 Alignment(std::vector<Boid>& neighbors)
+    {
+        glm::vec3 avgHeading = {};
+        size_t neighborCount = {};
+
+        for (size_t i = 0; i < neighbors.size(); i++)
+        {
+            if (*this == neighbors[i] && glm::length(m_position - neighbors[i].m_position) > m_neighborRange)
+            {
+                continue;
+            }
+
+            avgHeading += neighbors[i].GetHeading();
+            ++neighborCount;
+        }
+
+        if (neighborCount > 0)
+        {
+            avgHeading /= neighborCount;
+            avgHeading -= GetHeading();
+        }
+
+        return avgHeading;
+    }
+
+    glm::vec3 Cohesion(std::vector<Boid>& neighbors)
+    {
+        glm::vec3 centerOfMass = {};
+        glm::vec3 avgForce = {};
+        size_t neighborCount = {};
+
+        for (size_t i = 0; i < neighbors.size(); i++)
+        {
+            if (*this == neighbors[i] && glm::length(m_position - neighbors[i].m_position) > m_neighborRange)
+            {
+                continue;
+            }
+
+            centerOfMass += neighbors[i].m_position;
+            ++neighborCount;
+        }
+
+        if (neighborCount > 0)
+        {
+            centerOfMass /= neighborCount;
+
+            // Seek Behavior
+            glm::vec3 desired = glm::normalize(centerOfMass - m_position) * m_maxVelocity;
+            glm::vec3 steer = desired - m_velocity;
+
+            avgForce = glm::normalize(steer);
+        }
+
+        return avgForce;
+    }
+
+    glm::vec3 GetHeading() const {
+        if (m_velocity != glm::vec3(0.0f))
+        {
+            return glm::normalize(m_velocity);
+        }
+        return glm::vec3(0.0f, 0.0f, -1.0f);
+    }
+
+    bool HasFeature(unsigned int feature) const {
+        return (m_features & feature) != 0;
+    }
+    void SetFeature(unsigned int feature) { m_features = feature; }
+
+    glm::vec3 m_position;
+    glm::vec3 m_velocity;
+
+    glm::vec3 m_wanderTarget;
+    glm::vec3 m_targetPos;
+    glm::vec3 m_fleePos;
+
+    Boid* m_targetBoid;
+    Boid* m_fleeBoid;
+
+    unsigned int m_features;
+
+    float m_maxVelocity;
+    float m_maxAccelerationForce;
+
+    float m_radius;
+
+    float m_neighborRange = 3.0f;
+
+    unsigned int m_id = -1;
+    static unsigned int ID;
 };
+
+unsigned int Boid::ID = 0;
+
+// ENABLE_BITMASK_OPERATORS(Boid::Feature);
